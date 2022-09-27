3857b8d56853d62ce570d81fb029157ee0173ac1
Paadafalws <paul.lockeex@gmail.com>
2022-09-27 18:43:24 -0300

    Create from ctypes.wintypes import MSG.py

---
+++ from ctypes.wintypes import MSG.py
@@ -0,0 +1,10 @@
+from ctypes.wintypes import MSG
+
+
+print ("ola mundo") # para comentar #
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+
+print(str)
